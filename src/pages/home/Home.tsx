a366863d47523dd755c886605baf8695e2aab348
Bianca <bianca.alves.0704@gmail.com>
2026-05-13 11:03:38 -0300

    crud de postagem com edicação e deleção

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,6 @@
+import ListaPostagens from "../../components/postagem/listapostagens/ListaPostagens";
+import ModalPostagem from "../../components/postagem/modalpostagem/ModalPostagem";
+
 function Home() {
   return (
     <>
@@ -8,12 +11,7 @@ function Home() {
             <p className="text-xl">Expresse aqui seus pensamentos e opiniões</p>
 
             <div className="flex justify-around gap-4">
-              <div
-                className="rouded text-white
-                border-white border-solid border-2 py-2 px-4"
-              >
-                Nova Postagem
-              </div>
+               <ModalPostagem />
               </div>
               </div>
 
@@ -27,6 +25,8 @@ function Home() {
           </div>
         </div>
       </div>
+
+       <ListaPostagens />
     </>
   );
 }
